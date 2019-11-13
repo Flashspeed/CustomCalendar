ac92a408874021040c44b8c9eaeb8297c7b4161c
Flashspeed <flashspeed@hotmail.co.uk>
2019-11-13 00:31:44 +0000

    Year and Month selector now works as expected.

--- app/src/main/java/com/ife/customcalendar/fragments/FragmentCalendar.kt
+++ app/src/main/java/com/ife/customcalendar/fragments/FragmentCalendar.kt
@@ -26,6 +26,8 @@ class FragmentCalendar : Fragment()
     private val selectedDates = mutableSetOf<LocalDate>()
     private val today = LocalDate.now()
     private val monthTitleFormatter = DateTimeFormatter.ofPattern("MMMM")
+    private val yearArray = arrayListOf<String>()
+
 
 
     override fun onCreateView(
@@ -40,12 +42,18 @@ class FragmentCalendar : Fragment()
     {
         super.onViewCreated(view, savedInstanceState)
 
+        for(i in Year.now().value..Year.now().value + 20)
+        {
+            yearArray.add(i.toString())
+        }
+
         val currentMonth = YearMonth.now()
-        val firstMonth = currentMonth.minusMonths(10)
-        val lastMonth = currentMonth.plusMonths(10)
+        val firstMonth = currentMonth.minusMonths(0)
+        val lastMonth = currentMonth.plusMonths(yearArray.count() * 12.toLong())
         val firstDayOfWeek = DayOfWeek.SUNDAY
 
         initMonthSpinner()
+        initYearSpinner()
 
         calendarView.setup(firstMonth, lastMonth, firstDayOfWeek)
         calendarView.scrollToMonth(currentMonth)
@@ -99,6 +107,8 @@ class FragmentCalendar : Fragment()
 
     private fun initMonthSpinner()
     {
+
+        /*** MONTH SPINNER **/
         ArrayAdapter.createFromResource(
             context!!,
             R.array.days_of_month,
@@ -107,7 +117,6 @@ class FragmentCalendar : Fragment()
             adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
             spinnerMonthSelector.adapter = adapter
         }
-
 
         spinnerMonthSelector.onItemSelectedListener = object : AdapterView.OnItemSelectedListener
         {
@@ -127,50 +136,73 @@ class FragmentCalendar : Fragment()
             {
                 if(loadedOnce)
                 {
-                    val selectedMonthAndYear = (YearMonth.now().month.ordinal + 1).toString()
+                    val currentMonth = (YearMonth.now().month.ordinal + 1).toString()
                         .padStart(2, '0')
 
                     calendarView.scrollToMonth(
-                        /* Always begin with selecting the current year and month*/
-                        YearMonth.parse(
-                            "${Year.now()}-${(YearMonth.now().month.ordinal + 1).toString().padStart(
-                                2,
-                                '0'
-                            )}"
-                        )
+                        /* On first load always begin with selecting the current year and month*/
+                        YearMonth.parse("${Year.now()}-${currentMonth}")
                     )
                     spinnerMonthSelector.setSelection(YearMonth.now().month.ordinal)
-                    Toast.makeText(
-                        context!!,
-                        "Selected position non index $selectedMonthAndYear",
-                        Toast.LENGTH_SHORT
-                    ).show()
 
                     loadedOnce = false
 
                 }
                 else
                 {
-                    val selectedMonthAndYear = (position + 1).toString().padStart(2, '0')
-
+                    val selectedMonth = (position + 1).toString().padStart(2, '0')
+                    val yearMonthToScrollTo = "${spinnerYearSelector.selectedItem}-${selectedMonth}"
                     calendarView.scrollToMonth(
-                        YearMonth.parse(
-                            //TODO make year dynamic instead of always using current year
-                            "${Year.now()}-${(position + 1).toString().padStart(
-                                2,
-                                '0'
-                            )}"
-                        )
+                        //TODO make year dynamic instead of always using current year
+                        YearMonth.parse(yearMonthToScrollTo)
                     )
-                    Toast.makeText(
-                        context!!,
-                        "Selected position non index $selectedMonthAndYear",
-                        Toast.LENGTH_SHORT
-                    ).show()
                 }
 
             }
 
+        }
+    }
+
+    private fun initYearSpinner()
+    {
+        /*** YEAR SPINNER **/
+        ArrayAdapter<String>(
+            context!!,
+            R.layout.simple_spinner_item,
+            yearArray
+        ).let { adapter ->
+            adapter.setDropDownViewResource(R.layout.simple_spinner_dropdown_item)
+            spinnerYearSelector.adapter = adapter
+        }
+
+        yearArray.forEach {
+            println("Year: $it")
+        }
+
+        spinnerYearSelector.onItemSelectedListener = object : AdapterView.OnItemSelectedListener
+        {
+            override fun onNothingSelected(parent: AdapterView<*>?)
+            {
+            }
+
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            )
+            {
+                val yearMonthToScrollTo = "${spinnerYearSelector.getItemAtPosition(position)}-${(spinnerMonthSelector.selectedItemPosition + 1).toString().padStart(
+                    2,
+                    '0'
+                )}"
+
+                Toast.makeText(context!!, yearMonthToScrollTo, Toast.LENGTH_SHORT).show()
+
+//                calendarView.scrollToMonth(YearMonth.parse("2020-01"))
+                calendarView.scrollToMonth(YearMonth.parse(yearMonthToScrollTo))
+
+            }
         }
     }
 
